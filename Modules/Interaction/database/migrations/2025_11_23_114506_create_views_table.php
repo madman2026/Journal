b4be727a94346839213c977547a3ca8b35d068ef
Mad Man <madmanprogrammer2024@gmail.com>
2026-02-16 15:42:48 +0330

    modify_user_id_column_on_views_table for nullable

--- Modules/Interaction/database/migrations/2025_11_23_114506_create_views_table.php
+++ Modules/Interaction/database/migrations/2025_11_23_114506_create_views_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('views', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->foreignId('user_id')->nullable()->constrained('users')->cascadeOnDelete();
             $table->morphs('viewable');
             $table->ipAddress();
             $table->timestamps();
---
+++ Modules/Interaction/database/migrations/2026_02_16_120650_modify_column_user_id_on_views_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('views', function (Blueprint $table) {
+            $table->foreignId('user_id')
+                  ->nullable()
+                  ->change();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('views', function (Blueprint $table) {
+            $table->foreignId('user_id')
+                  ->nullable(false)
+                  ->change();
+        });
+    }
+};
+
